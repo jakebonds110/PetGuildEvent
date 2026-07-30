67565aed2cfecde808f120acc9e8cd3c92ac0967
jakebonds110 <jakebonds110@gmail.com>
2026-07-30 17:39:34 -0400

    Add files via upload

---
+++ AutoExec.lua
@@ -0,0 +1,11 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jakebonds110/RainbowCarpetAuto/refs/heads/main/EquipRainbowCarpet.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+
+
+-- task.wait(14)
+-- loadstring(readfile("AutoDropPet.lua"))()
+
+loadstring(readfile("Tracker.lua"))()
+loadstring(readfile("Tracker.lua"))()
+-- script_key = "OdZSSLOXrvEBjRDObvnjtOAmWISuTSny"
