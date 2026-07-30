bf5f854747f092ebdbf0c61abd3b4393967142c4
jakebonds110 <jakebonds110@gmail.com>
2026-07-30 17:44:44 -0400

    Update AutoExec.lua

--- AutoExec.lua
+++ AutoExec.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/jakebonds110/RainbowCarpetAuto/refs/heads/main/EquipRainbowCarpet.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jakebonds110/PetGuildEvent/refs/heads/main/EquipRainbowCarpet.lua", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/jakebonds110/RainbowCarpetAuto/refs/heads/main/Tracker.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jakebonds110/PetGuildEvent/refs/heads/main/Tracker.lua", true))()
