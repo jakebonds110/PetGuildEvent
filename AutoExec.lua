71ad718fe32fd1d630e6847903b6dec87963f9cf
jakebonds110 <jakebonds110@gmail.com>
2026-07-30 17:41:43 -0400

    Add files via upload

---
+++ AutoExec.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jakebonds110/RainbowCarpetAuto/refs/heads/main/EquipRainbowCarpet.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jakebonds110/RainbowCarpetAuto/refs/heads/main/Tracker.lua", true))()
