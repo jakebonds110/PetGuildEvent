8cbc30bcedf837eed875aa77c12cfea32e2f0f50
jakebonds110 <jakebonds110@gmail.com>
2026-07-31 09:38:30 -0400

    Create MultipleAccAutoExec.lua

---
+++ MultipleAccAutoExec.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jakebonds110/PetGuildEvent/refs/heads/main/EquipRainbowCarpet.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jakebonds110/PetGuildEvent/refs/heads/main/MultipleAccTracker.lua", true))()
